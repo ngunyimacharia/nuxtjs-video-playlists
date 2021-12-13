363ce3ecd9c817dffec97bb310d2c8bf7cceecec
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2021-12-13 16:13:50 +0200

    Fixed source playlist complete

---
+++ components/FixedSourcePlaylist.vue
@@ -0,0 +1,45 @@
+<template>
+    <video
+        id="fixed-source-player"
+        controls
+        autoplay
+        class="cld-video-player cld-video-player-skin-dark w-1/2 mx-auto h-96"
+    >
+    </video>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            cld: null,
+            player:null,
+            sources:[
+                "nuxtjs-video-playlist/pexels-taryn-elliott-9116112.mp4",
+                "nuxtjs-video-playlist/production_ID_3987730.mp4",
+                "nuxtjs-video-playlist/pexels-zlatin-georgiev-7173031.mp4",
+                "nuxtjs-video-playlist/Pexels_Videos_1526909.mp4",
+                "nuxtjs-video-playlist/pexels-taryn-elliott-5220279.mp4"
+            ]
+        }
+    },
+    mounted(){
+        this.cld = cloudinary.Cloudinary.new({ cloud_name: process.env.NUXT_ENV_CLOUDINARY_CLOUD_NAME, secure: true});
+
+        this.player = this.cld.videoPlayer(
+            "fixed-source-player",
+             {
+                playlistWidget: {
+                    direction:'vertical',
+                    total:5
+                }
+             }
+        );
+
+        this.player.playlist(
+            this.sources, 
+            { autoAdvance: true, repeat: true, presentUpcoming: 8 }
+        );
+    }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1 class="text-center text-2xl leading-8 font-extrabold tracking-tight text-gray-700 m-12 ">Fixed source playlist</h1>
+    <fixed-source-playlist class="m-24"/>
+  </div>
 </template>
 
 <script>
+import FixedSourcePlaylist from '../components/FixedSourcePlaylist.vue'
 export default {
+  components: { FixedSourcePlaylist },
   name: 'IndexPage'
 }
 </script>
